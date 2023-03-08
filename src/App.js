5bf0b50985895703b782f3ae53408844a50af87f
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-08 16:06:50 -0500

    Hacemos fetch a la api y enviamos los datos al pokemonList, mostramos el ícono y hacemos la conexión de nuestra app con la store mediante el uso del mapState y mapDispatch.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import { Col } from 'antd';
+import { connect } from 'react-redux';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
+import { useEffect, useState } from 'react';
+import { getPokemon } from './api';
+import { setPokemons as setPokemonsActions } from './actions';
+import logo from './statics/logo.svg';
 import './App.css';
 
-function App() {
+function App({ pokemons, setPokemons }) {
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const pokemonRes = await getPokemon();
+      setPokemons(pokemonRes);
+    };
+    fetchPokemons();
+  }, []);
+
   return (
     <div className="App">
+      <Col span={4} offset={10}>
+        <img src={logo} alt='Pokedux' />
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList />
+      <PokemonList pokemons={pokemons} />
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsActions(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
