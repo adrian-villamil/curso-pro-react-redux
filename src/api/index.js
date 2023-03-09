e71b8f76824c22ad4de831dca0e99dd2ae12a35b
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-09 15:06:29 -0500

    Creamos otra función para hacer fetch a la información detallada de un pokemon en específico.

--- src/api/index.js
+++ src/api/index.js
@@ -4,4 +4,10 @@ export const getPokemon = () => {
   return axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
     .then(response => response.data.results)
     .catch(error => console.error(error));
-}
+};
+
+export const getPokemonDetails = (pokemon) => {
+  return axios.get(pokemon.url)
+    .then(response => response.data)
+    .catch(error => console.error(error));
+};
