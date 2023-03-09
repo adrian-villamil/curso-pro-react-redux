2d283e25d864b412c51d7c56abec0ed50a880b61
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-09 15:07:05 -0500

    Quitamos el middleware de mostrar siempre nuestro pokemon personalizado.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import App from './App';
 import { pokemonsReducer } from './reducers/pokemons';
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
-import { featuring, logger } from './middlewares';
+import { logger } from './middlewares';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const composedEnhancers = compose(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(logger, featuring)
+  applyMiddleware(logger)
 );
 
 const store = createStore(pokemonsReducer, composedEnhancers);
