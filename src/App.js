71c517a2966d0edfb35e2bd0517a1367907884e2
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-09 15:07:29 -0500

    Ahora agregamos todos los pokemones con su información detallada.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Col } from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import { useEffect } from 'react';
-import { getPokemon } from './api';
+import { getPokemon, getPokemonDetails } from './api';
 import { setPokemons } from './actions';
 import { useDispatch, useSelector } from 'react-redux';
 import logo from './statics/logo.svg';
@@ -16,7 +16,8 @@ function App() {
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonRes = await getPokemon();
-      dispatch(setPokemons(pokemonRes));
+      const pokemonDetailed = await Promise.all(pokemonRes.map(pokemon => getPokemonDetails(pokemon)));
+      dispatch(setPokemons(pokemonDetailed));
     };
     fetchPokemons();
   }, []);
