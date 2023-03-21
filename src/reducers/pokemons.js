280a12e3641fd7c15efc4c92d1d940d47c4101c2
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-21 16:46:29 -0500

    En nuestro reducer, utilizamos built-in functions de la librería immutable para obtener y asignar los valores de nuestros estados.

--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,34 +1,29 @@
+import { fromJS, getIn, setIn } from "immutable";
 import { SET_FAVORITE, SET_LOADING, SET_POKEMONS } from "../actions/types";
 
-const initialState = {
+const initialState = fromJS({
   pokemons: [],
   loading: false,
-};
+});
 
 export const pokemonsReducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case SET_POKEMONS:
-      return {
-        ...state,
-        pokemons: action.payload
-      };
+      // return { ...state, pokemons: action.payload };
+      return setIn(state, ['pokemons'], fromJS(action.payload));
     case SET_FAVORITE:
-      const newPokemonList = [...state.pokemons];
-      const currentPokemonIndex = newPokemonList.findIndex(pokemon => {
-        return pokemon.id === action.payload.pokemonId;
+      const currentPokemonIndex = state.get('pokemons').findIndex(pokemon => {
+        return pokemon.get('id') === action.payload.pokemonId;
       });
 
       if (currentPokemonIndex < 0) {
         return state;
       }
 
-      newPokemonList[currentPokemonIndex].favorite = !newPokemonList[currentPokemonIndex].favorite;
-      return {...state, pokemons: newPokemonList}
+      const isFavorite = getIn(state, ['pokemons', currentPokemonIndex, 'favorite']);
+      return setIn(state, ['pokemons', currentPokemonIndex, 'favorite'], !isFavorite);
     case SET_LOADING:
-      return {
-        ...state,
-        loading: action.payload
-      }
+      return setIn(state, ['loading'], fromJS(action.payload));
     default:
       return state;
   }
