0d2ad42b4123fae4ce49729f7b95e86968066b4d
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-20 23:29:25 -0500

    Traemos el estado loading y mostramos un spinner siempre y cuando se está haciendo el fetch a la api.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Col } from 'antd';
+import { Col, Spin } from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import { useEffect } from 'react';
 import { getPokemon } from './api';
-import { getPokemonWithDetails } from './actions';
+import { getPokemonWithDetails, setLoading } from './actions';
 import { useDispatch, useSelector } from 'react-redux';
 import logo from './statics/logo.svg';
 import './App.css';
@@ -11,12 +11,15 @@ import './App.css';
 function App() {
 
   const pokemons = useSelector(state => state.pokemons);
+  const loading = useSelector(state => state.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemons = async () => {
+      dispatch(setLoading(true));
       const pokemonRes = await getPokemon();
       dispatch(getPokemonWithDetails(pokemonRes));
+      dispatch(setLoading(false));
     };
     fetchPokemons();
   }, []);
@@ -29,7 +32,17 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons} />
+      {
+        loading ?
+          (
+            <Col offset={12}>
+              <Spin spinning={loading} size='large' />
+            </Col>
+          ) :
+          (
+            <PokemonList pokemons={pokemons} />
+          )
+      }
     </div>
   );
 };
