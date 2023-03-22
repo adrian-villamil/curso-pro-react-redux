85fb34257b4b9532c41f7d7f9cd573ec3f4c2764
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-21 19:06:56 -0500

    Separamos las responsabilidades en dos reducers, uno maneja la información (los pokemones), y el otro el ui (loading).

--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,15 +1,13 @@
 import { fromJS, getIn, setIn } from "immutable";
-import { SET_FAVORITE, SET_LOADING, SET_POKEMONS } from "../actions/types";
+import { SET_FAVORITE, SET_POKEMONS } from "../actions/types";
 
 const initialState = fromJS({
   pokemons: [],
-  loading: false,
 });
 
 export const pokemonsReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_POKEMONS:
-      // return { ...state, pokemons: action.payload };
       return setIn(state, ['pokemons'], fromJS(action.payload));
     case SET_FAVORITE:
       const currentPokemonIndex = state.get('pokemons').findIndex(pokemon => {
@@ -22,8 +20,6 @@ export const pokemonsReducer = (state = initialState, action) => {
 
       const isFavorite = getIn(state, ['pokemons', currentPokemonIndex, 'favorite']);
       return setIn(state, ['pokemons', currentPokemonIndex, 'favorite'], !isFavorite);
-    case SET_LOADING:
-      return setIn(state, ['loading'], fromJS(action.payload));
     default:
       return state;
   }
