aa200d85670edb15070b7661bebb04e99bf24b82
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-26 23:22:05 -0500

    Agregamos el fetch de la información.

--- src/slices/dataSlice.js
+++ src/slices/dataSlice.js
@@ -1,8 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { setLoading } from "../slices/uiSlice";
+import { getPokemon, getPokemonDetails } from "../api";
 
 const initialState = {
   pokemons: [],
 };
+
+export const fetchPokemonsWithDetails = createAsyncThunk(
+  'data/fetchPokemonsWithDetails',
+  async (_, { dispatch }) => {
+    dispatch(setLoading(true));
+    const pokemonRes = await getPokemon();
+    const pokemonDetailed = await Promise.all(pokemonRes.map(pokemon => (
+      getPokemonDetails(pokemon)
+    )));
+    dispatch(setPokemons(pokemonDetailed));
+    dispatch(setLoading(false));
+  }
+);
 
 export const dataSlice = createSlice({
   name: 'data',
