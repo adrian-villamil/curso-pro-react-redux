c6ed05dcce7d2f4390ac945fd047f7ac9a582fea
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-26 23:25:23 -0500

    Modificamos el import del action setFavorite, que ahora se encuentra dentro del slice dataSlice.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,7 @@
 import { Card } from "antd";
 import Meta from "antd/es/card/Meta";
 import { useDispatch } from "react-redux";
-import { setFavorite } from "../actions";
+import { setFavorite } from "../slices/dataSlice";
 import StarButton from "./StarButton";
 
 const PokemonCard = ({ name, image, types, id, favorite }) => {
